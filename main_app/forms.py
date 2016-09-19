a0154ea650befdca9ba52e53c748b0a80c120cb8
aml007 <albertomartinezlope@gmail.com>
2016-09-19 13:08:01 +0200

    Added Image Field to upload images

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,11 +4,11 @@ from .models import Treasure
 class TreasureForm(forms.ModelForm):
   class Meta:
     model = Treasure
-    fields = ['name', 'value', 'location', 'material', 'img_url']
+    fields = ['name', 'value', 'location', 'material', 'image']
     widgets = {
       'name': forms.TextInput(attrs={'class': 'form-control'}),
       'value': forms.NumberInput(attrs={'class': 'form-control'}),
       'location': forms.TextInput(attrs={'class': 'form-control'}),
       'material': forms.TextInput(attrs={'class': 'form-control'}),
-      'img_url': forms.TextInput(attrs={'class': 'form-control'})
+      'image': forms.FileInput(attrs={'class': 'form-control'})
     }
