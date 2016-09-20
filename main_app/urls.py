ef0e10a3b1899801ca8a5088d4c3c12a17d3c847
aml007 <albertomartinezlope@gmail.com>
2016-09-20 21:34:05 +0200

    Added User model

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
-  url(r'^([0-9]+)$', views.detail, name = 'detail'),
-  url(r'^post_url/$', views.post_treasure, name = 'post_treasure')
+  url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+  url(r'^post_url/$', views.post_treasure, name = 'post_treasure'),
+  url(r'^user/(\w+)/$', views.profile, name = 'profile')
 ]
 
 # Add to the bottom of file
